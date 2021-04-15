370d355e8fa7ea07dad12a6222cda7218157e79b
mzdase902 <mzdase902@gmail.com>
2021-04-15 19:33:48 +0300

    hw

---
+++ HomeWork/Homework_5.js
@@ -0,0 +1,17 @@
+const ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep() {
+      alert( this.step );
+      return this;
+    }
+  }
+  
+ladder.up().up().up().up().down().showStep()
