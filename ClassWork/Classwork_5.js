31b485f093ee023e4389261177e78ac141918ebd
mzdase902 <mzdase902@gmail.com>
2021-06-01 03:08:42 +0300

    hw11

---
+++ ClassWork/Classwork_5.js
@@ -0,0 +1,35 @@
+// let i = 0;
+
+// const interval = setInterval(() => {
+//     if (i === 10) {
+//         clearInterval (interval);
+//     }
+//     i++
+//     console.log (i)
+// }, 500);
+
+
+
+// let printNumbers = (from, to) => {
+//     let i = from;
+  
+//     let interval = setInterval(() => {
+//       if(i === to) {
+//         clearInterval(interval);
+//       }
+//       console.log(i++)
+//     }, 1000);
+//   }
+  
+//   printNumbers(2, 4)
+
+// function setTime(from, to) {
+//     let timer = setTimeout(function run() {
+//       if (from > to) {
+//         clearTimeout(timer);
+//       } else {
+//         console.log(from++);
+//         setTimeout(run, 1000);
+//       }
+//     }, 1000);
+//   }
